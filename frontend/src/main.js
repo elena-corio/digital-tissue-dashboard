3dbb120f2b8c9bfbe346f897683242dfe71478fe
elena-code-star <elena.corio@students.iaac.net>
2026-03-14 02:13:11 +0100

    use urql and speckle client in main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue';
 import './styles/style.css';
 import App from './App.vue';
 import router from './router.js';
+import urql from '@urql/vue'
+import { speckleClient } from './services/speckleClient';
 
 const app = createApp(App);
 app.use(router);
+app.use(urql, speckleClient)
 app.mount('#app');
