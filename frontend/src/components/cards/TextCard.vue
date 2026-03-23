b1d5b8bf5be8e8bbe8e2fdb75b123d67ac4e9b05
elena-code-star <elena.corio@students.iaac.net>
2026-03-23 01:36:26 +0100

    Add tiny detail in text cards

--- frontend/src/components/cards/TextCard.vue
+++ frontend/src/components/cards/TextCard.vue
@@ -21,6 +21,7 @@ defineProps({
 .text-card {
   padding: var(--space-lg);
   color: var(--navy-100);
+  font-weight: var(--font-weight-medium);
   width: 100%;
 }
 
@@ -32,13 +33,23 @@ defineProps({
   text-align: center;
 }
 
-.card-list-item {
-  font-size: var(--font-size-body);
-  line-height: var(--line-height-relaxed);
-  margin-bottom: var(--space-sm);
-}
+  .card-list-item {
+    font-size: var(--font-size-body);
+    line-height: var(--line-height-relaxed);
+    margin-bottom: var(--space-sm);
+    position: relative;
+  }
+
+  .card-list-item:not(:last-child)::after {
+    content: "";
+    display: block;
+    margin: 12px auto 0 auto;
+    width: 12%;
+    border-bottom: 2px solid var(--light-blue-100);
+  }
 
 .card-list-item:last-child {
   margin-bottom: 0;
+  border-bottom: none;
 }
 </style>
